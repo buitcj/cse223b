7f649bdef2c3d1b1f796f549e952cfcb893b23a8
Julian Bui <julianbui@gmail.com>
2013-04-22 05:30:43 -0700

    implemented first two methods

--- lab2/lab2/src/Tribbler_server.cpp
+++ lab2/lab2/src/Tribbler_server.cpp
@@ -29,6 +29,9 @@ using namespace KeyValueStore;
 
 class TribblerHandler : virtual public TribblerIf {
  public:
+    
+  const string USER_PREFIX = "jbu_user_";
+  const string SET_PREFIX = "jbu_set_";
 
   TribblerHandler(std::string storageServer, int storageServerPort) {
     // Your initialization goes here
@@ -39,77 +42,90 @@ class TribblerHandler : virtual public TribblerIf {
   TribbleStatus::type CreateUser(const std::string& userid) {
     // Your implementation goes here
     printf("CreateUser\n");
-    string key = "user_list_jbu";
-    GetListResponse glr = GetList(key); 
-    if(glr.status != KVStoreStatus::OK)
-    {
-        // first time: we need to insert the list
-        // just add the list
 
-        // try to add it to the list
-        KVStoreStatus::type kvss = AddToList(key, userid);
-        if(kvss == KVStoreStatus::OK)
-        {
-            return TribbleStatus::OK;
-        }
-        else
-        {
-            // wtf ??? no corresponding error message *********************
-            return TribbleStatus::NOT_IMPLEMENTED;
-        }
+    // determine if user exists, if not create the json, put the json
+
+    // determine if user already exists
+    KeyValueStore::GetResponse get_ret_val = Get(userid);
+    if(get_ret_val.status == KVStoreStatus::OK || get_ret_val.value.length() != 0)
+    {
+       return TribbleStatus::EEXISTS; 
+    } 
+
+    // user does not exist, so create the json and put it
+    Json::Value root;
+    root["curSetNum"] = 0;
+    root["listOfSubscribers"] = Json::nullValue; 
+    
+    Json::StyledWriter writer;
+    string value = writer.write(root);
+
+    string key = string("jbu_user_").append(userid);
+
+    // put the json
+    KVStoreStatus::type put_ret_val = Put(key, value);
+
+    if(put_ret_val == KVStoreStatus::OK)
+    {
+        return TribbleStatus::OK;
     }
     else
     {
-        // list exists
-        
-        // check if user exists
-        vector<string>& values= glr.values;
-        vector<string>::iterator iter;
-        bool found = false;
-        for(iter = values.begin(); iter != values.end(); iter++)
-        {
-            if(iter->compare(userid) == 0)
-            {
-                found = true;
-                break;
-            }
-        }
-
-        // user exists, don't do anything
-        if(found == true)
-        {
-            return TribbleStatus::EEXISTS;
-        }
-        // user doesn't exist, add it to the list
-        else
-        {
-            // try to add it to the list
-            KVStoreStatus::type kvss = AddToList(key, userid);
-            if(kvss == KVStoreStatus::OK)
-            {
-                return TribbleStatus::OK;
-            }
-            else
-            {
-                // wtf ??? no corresponding error message *********************
-                return TribbleStatus::NOT_IMPLEMENTED;
-            }
-        }
+       return TribbleStatus::NOT_IMPLEMENTED; 
     }
+
+    /*
+    KVStoreStatus::type Put(std::string key, std::string value) {
+    KeyValueStore::GetResponse Get(std::string key)
+    GetListResponse glr = GetList(key); 
+    if(glr.status != KVStoreStatus::OK)
+        KVStoreStatus::type kvss = AddToList(key, userid);
+    */
   }
 
   TribbleStatus::type AddSubscription(const std::string& userid, const std::string& subscribeto) {
     // Your implementation goes here
     printf("AddSubscription\n");
 
-    Json::Value root;
-    root["timestamp"] = Json::Value(555);
+    // get the user info, parse the json, add the subscription, put the json
 
+    KeyValueStore::GetResponse get_ret_val = Get(string(USER_PREFIX).append(userid));
+    if(get_ret_val.status == KVStoreStatus::EKEYNOTFOUND ||
+       get_ret_val.status == KVStoreStatus::EITEMNOTFOUND) // NOT SURE WE NEED THIS*************************************** 
+    {
+        return TribbleStatus::INVALID_USER;
+    }
+
+    KeyValueStore::GetResponse get_subscribe_ret_val = Get(string(USER_PREFIX).append(subscribeto)); 
+    if(get_subscribe_ret_val.status == KVStoreStatus::EKEYNOTFOUND ||
+       get_subscribe_ret_val.status == KVStoreStatus::EITEMNOTFOUND) // NOT SURE WE NEED THIS*************************************** 
+    {
+        return TribbleStatus::INVALID_SUBSCRIBETO;
+    }
+
+    Json::Value user_info;
+    Json::Reader reader;
+    bool parse_ret_value = reader.parse(get_ret_val.value, user_info);
+    if(parse_ret_value)
+    {
+        return TribbleStatus::NOT_IMPLEMENTED; //***************   
+    }
+    const Json::Value list_of_subscribers = user_info["listOfSubscribers"];
+    Json::Value list_of_subscribers_copy = list_of_subscribers;
+    Json::Value new_list_of_subscribe_tos = list_of_subscribers_copy.append(subscribeto);
+    user_info["listOfSubscribers"] = new_list_of_subscribe_tos;
 
     Json::StyledWriter writer;
-    string output_json = writer.write(root);
+    string value = writer.write(user_info);
 
-    return TribbleStatus::NOT_IMPLEMENTED;
+    // put the json
+    KVStoreStatus::type put_ret_val = Put(string(USER_PREFIX).append(userid), value);
+    if(put_ret_val != KVStoreStatus::OK)
+    {
+        return TribbleStatus::NOT_IMPLEMENTED; //*************
+    } 
+
+    return TribbleStatus::OK;
   }
 
   TribbleStatus::type RemoveSubscription(const std::string& userid, const std::string& subscribeto) {
